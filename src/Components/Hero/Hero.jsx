ef2f4b797a97c555e42a4efd7fcf8b888c773b08
Mohamed-Aseem <ima7str@gmail.com>
2024-03-17 09:36:36 +0000

    Create Hero Component and Modify

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        
+    <div className='container hero'>
+        <div className="hero-text">
+          <h1>We guarantee improved learning for a better society</h1>
+          <p>Our innovative curriculum is developed to give students the information, 
+            skills, and experiences they need to succeed in the fast-paced world of education.
+          </p>
+          <button className='btn'>More Details <img src={dark_arrow} alt="" /></button>
+        </div>
     </div>
   )
 }
