217692c1017c6ad5d0be53f2d28fc9552d68a8b3
Mohamed-Aseem <156054223+Mohamed-Aseem@users.noreply.github.com>
2024-03-18 17:05:13 +0000

    Update Programs.jsx With AOS

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect }from 'react'
 import './Programs.css'
 import program_1 from '../../assets/program-1.png' 
 import program_2 from '../../assets/program-2.png' 
@@ -6,25 +6,33 @@ import program_3 from '../../assets/program-3.png'
 import program_icon_1 from '../../assets/program-icon-1.png'
 import program_icon_2 from '../../assets/program-icon-2.png'
 import program_icon_3 from '../../assets/program-icon-3.png'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 
 const Programs = () => {
+
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  },[])
+
   return (
     <div className='programs'>
-      <div className="program">
+      <div className="program" data-aos='fade-left' data-aos-duration='2000'>
         <img src={program_1} alt="" />
         <div className="caption">
             <img src={program_icon_1} alt="" />
             <p>Graduation Degree</p>
         </div>
       </div>
-      <div className="program">
+      <div className="program" data-aos='fade-up' data-aos-duration='2000'>
         <img src={program_2} alt="" />
         <div className="caption">
             <img src={program_icon_2} alt="" />
             <p>Master Degree</p>
         </div>
       </div>
-      <div className="program">
+      <div className="program" data-aos='fade-right' data-aos-duration='2000'>
         <img src={program_3} alt="" />
         <div className="caption">
             <img src={program_icon_3} alt="" />
