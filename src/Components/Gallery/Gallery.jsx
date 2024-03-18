bc909b57f5039b9cadd5105b6e1a0f1a228ca8b2
Mohamed-Aseem <156054223+Mohamed-Aseem@users.noreply.github.com>
2024-03-18 17:04:12 +0000

    Update Gallery.jsx With AOS

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useEffect }from 'react'
 import './Gallery.css'
 import gallery_1 from '../../assets/gallery-1.png'
 import gallery_2 from '../../assets/gallery-2.png'
 import gallery_3 from '../../assets/gallery-3.png'
 import gallery_4 from '../../assets/gallery-4.png'
 import white_arrow from '../../assets/white-arrow.png'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 function Gallery() {
+
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  },[])
+
   return (
     <div className='galleries'>
         <div className="gallery">
-            <img src={gallery_1} alt="" />
-            <img src={gallery_2} alt="" />
-            <img src={gallery_3} alt="" />
-            <img src={gallery_4} alt="" />
+            <img src={gallery_1} alt="" data-aos='fade-in' data-aos-duration='2000'/>
+            <img src={gallery_2} alt="" data-aos='fade-in' data-aos-duration='2000' data-aos-delay="200"/>
+            <img src={gallery_3} alt="" data-aos='fade-in' data-aos-duration='2000' data-aos-delay="400"/>
+            <img src={gallery_4} alt="" data-aos='fade-in' data-aos-duration='2000' data-aos-delay="600"/>
         </div>
-        <button className='btn dark-btn'>See More Here <img src={white_arrow} alt="" /></button>
+        <button className='btn dark-btn' data-aos='slide-up' data-aos-offset="-100" data-aos-duration='2000'>See More Here <img src={white_arrow} alt="" /></button>
     </div>
   )
 }
