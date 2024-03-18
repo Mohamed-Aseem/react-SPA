001f7b55cc0a8511660362972972d124813301e0
Mohamed-Aseem <ima7str@gmail.com>
2024-03-18 05:42:29 +0000

    Create Gallery Component and Modify App.jsx and Index.css Files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Gallery from './Components/Gallery/Gallery'
 
 const App = () => {
   return (
@@ -14,6 +15,8 @@ const App = () => {
         <Title subTitle='Our Programs' title='What We Offer'/>
         <Programs />
         <About />
+        <Title subTitle='Gallery' title='Campus Photos'/>
+        <Gallery />
       </div>
     </div>
   )
